0ef91191760cb84162710d4dde4fbe331ef5ba12
Sumit Kumawat <kumawatsumit199@gmail.com>
2024-09-08 21:21:15 +0530

    new sections add

--- javascript/custom.js
+++ javascript/custom.js
@@ -3,3 +3,24 @@ let loader = document.getElementById("preloader");
 function myPreloderFunction() {
   loader.style.display = "none";
 }
+
+// tap to scroll
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    1000
+  );
+});
+
+
+
+
+//dark mode
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
